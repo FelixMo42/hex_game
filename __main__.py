206e656a0313f06eb16682b4f1ecf686e58909da
FelixMo42 <felix@tacocat.com>
2021-05-22 17:35:04 -0700

    add basic pyglet setup

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import pyglet
+
+# initlize a new window
+window = pyglet.window.Window(800, 600)
+
+@window.event
+def on_draw():
+    # make sure there is no trace of privous stuff left behing
+    window.clear()
+
+
+if __name__ == '__main__':
+    pyglet.app.run()
