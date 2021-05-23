782fb368ae1c6d438c58ae1061cc96deaf489be2
FelixMo42 <felix@tacocat.com>
2021-05-23 15:36:17 -0700

    moved callback to Window subclass and added more callbacks

--- __main__.py
+++ __main__.py
@@ -1,13 +1,19 @@
 import pyglet
 
-# initlize a new window
-window = pyglet.window.Window(800, 600,resizable=True)
+class GameWindow(pyglet.window.Window) :
+    def on_draw(self) :
+        # make sure there is no trace of privous stuff left behing
+        window.clear()
 
-@window.event
-def on_draw():
-    # make sure there is no trace of privous stuff left behing
-    window.clear()
+    def on_key_press(self, key, mod) :
+        pass
 
+    def on_mouse_press(self, x, y, button, mod) :
+        pass
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
+    # initlize a new window
+    pyglet.window.Window(800, 600,resizable=True)
+
+    # start the actuall game
     pyglet.app.run()
